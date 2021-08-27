69acd79d94c72e31814062a57a2527f51c5e243e
Adnan Zareef <adnanzareef@outlook.com>
2021-08-27 12:02:27 +0600

    JS functions isolated & minor style+element change

---
+++ assets/js/functions.js
@@ -0,0 +1,7 @@
+function avatarChange() {
+	document.getElementById("avatar").src = "assets/img/avatars/hello.gif";
+}
+
+function avatarRestore() {
+	document.getElementById("avatar").src = "assets/img/avatars/avatar.jpg";
+}
